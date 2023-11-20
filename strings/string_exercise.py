546241a50e723e5649600a602bc152d5aaea9f1d
Jakob Kaur <jakob-kaur.kelder@voco.ee>
2023-11-20 09:03:49 +0200

    sõne

---
+++ strings/string_exercise.py
@@ -0,0 +1,7 @@
+# Write your code here
+first_name = "James"
+last_name = "Bond"
+full_name = first_name + last_name
+self_description_sentence = "my name is " + last_name + ", " + first_name + last_name
+cake = [[vahukoor][marjad][täidis][põhi]]
+print(\n cake)
