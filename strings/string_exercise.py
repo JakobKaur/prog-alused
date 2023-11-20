9a02c61ecd2fff954049d29251fa2a91446037b3
Jakob Kaur <jakob-kaur.kelder@voco.ee>
2023-11-20 09:06:04 +0200

    sõne

--- strings/string_exercise.py
+++ strings/string_exercise.py
@@ -1,7 +1,12 @@
 # Write your code here
 first_name = "James"
 last_name = "Bond"
-full_name = first_name + last_name
-self_description_sentence = "my name is " + last_name + ", " + first_name + last_name
-cake = [[vahukoor][marjad][täidis][põhi]]
-print(\n cake)
+full_name = first_name + " " + last_name
+self_description_sentence = "My name is " + last_name + ", " + full_name + "."
+cake = "vahukoormarjadtäidispõhi"
+cake = "vahukoormarjadtäidispõhi"
+print(cake[:8] + "\n" + cake[8:14] + "\n" + cake[14:20] + "\n" + cake[20:])
+original_string = "Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string.split()[0][::-1]
